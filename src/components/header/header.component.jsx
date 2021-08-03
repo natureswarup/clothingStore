4bb48127c7ed8a4fa990e7686af2e64dc564d6da
Nature <nature.swarup@gmail.com>
2021-08-03 16:48:13 -0400

    added cart-icon and cart dropdown components to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect} from 'react-redux'
+import { connect} from 'react-redux';
+
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import {auth} from '../../firebase/firebase.utils'
+import CartIcon  from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
@@ -26,7 +29,9 @@ const Header = ({currentUser}) => (
         :
         <Link className='option' to='/signin'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
